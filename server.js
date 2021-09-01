901ebc094b491da1e8d3bb8da8753a98b0442847
sahil-777 <turbothunderboltz@gmail.com>
2021-09-02 04:38:53 +0530

    express-session pkg added

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors')
+const session = require('express-session');
 
 let app = express();
 app.use(cors());
@@ -16,11 +17,24 @@ app.use(bodyParser.json());
 app.use(express.json({}));
 dotenv.config();
 
+app.set('trust proxy', 1);
+const secretKey = process.env.SECRET_SESSION_KEY;
+app.use(session({
+    secret: secretKey,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        maxAge: 3600000 // 1 hr
+    }
+}))
+
 app.get('/', async (req, res) => {
+    req.session.UID = 1; //test
     return res.render('home-page');
 })
 
 app.get('/login-page', async (req, res) => {
+    console.log(req.session); //test
     return res.render('login-page');
 })
 
