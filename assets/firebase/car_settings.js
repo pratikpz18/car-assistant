15bea372f205c92a81337bd7587e88b25d60b5df
Pratik Zinjurde <zinjurdepratik.pz@gmail.com>
2021-09-29 20:54:48 +0530

    added remove car and add new car feature

---
+++ assets/firebase/car_settings.js
@@ -0,0 +1,72 @@
+
+var snap,data,idNumber;
+firebase.auth().onAuthStateChanged(async (user) => {
+  if (user) {
+      const UID = user.uid;
+      const snapshot = await firebase.database().ref(`carOwners/${UID}`).once('value')
+      snap = await firebase.database().ref(`carOwners/${UID}/allCars`);
+      data = snapshot.val();
+      idNumber = Object.keys(data.allCars ? data.allCars : 0 ).length ;
+      showCars(data);
+  }
+})
+
+var details=[];
+function showCars(data){
+  //to add data dynamically
+  console.log(data.allCars)
+  for(let i=0;i<idNumber;i++){
+    details.push(Object.values(data.allCars)[i]);
+
+    var total = document.createElement('div');
+    total.classList.add("card-body");
+    total.setAttribute('id', `${details[i].carDetails.vehicleNumber}`);
+
+    var contentdiv = document.createElement('div');
+    contentdiv.innerHTML= `<div class="card-body">
+                            <div class="card-body">
+                              <p class="text-primary">${details[i].carDetails.brand} ${details[i].carDetails.model}</p>
+                            </div>
+                            <div class="d-flex justify-content-end">
+                              <button type="submit" onclick="removeCar(${i})" id="btn" class="btn btn-danger">Remove Car</button>
+                            </div>
+                          </div>`;
+
+    total.appendChild(contentdiv);
+    document.getElementById("carDetails").appendChild(total); 
+  }
+}
+
+async function removeCar(i) {
+  console.log(details[i].carDetails.vehicleNumber);
+  await snap.child(details[i].carDetails.vehicleNumber).remove();
+  let element = document.getElementById(details[i].carDetails.vehicleNumber);
+  element.parentNode.removeChild(element);
+};
+
+function AddNewCarDetails() {
+  let brand = document.getElementById("brand").value;
+  let model = document.getElementById("model").value;
+  let vehicleNumber = document.getElementById("vehicleNumber").value;
+  console.log(brand,model);
+  let cardetails = {
+    allStatus:{
+      batteryVoltage:14,
+      engineTemp:50,
+      suspensionLevel:50,
+    },
+    carDetails:{
+      brand:brand,
+      model:model,
+      vehicleNumber:vehicleNumber,
+    }
+  }
+  if(brand.value!="" && model.value!=""){
+    snap.child(`${vehicleNumber}`).set(cardetails);
+  }else{
+    console.log("put values")
+  }
+  brand.value="";
+  model.value="";
+  location.reload();
+}
--- assets/firebase/index.js
+++ assets/firebase/index.js
@@ -1,17 +1,84 @@
 firebase.auth().onAuthStateChanged(async (user) => {
     if (user) {
-        const UID = user.uid
+        const UID = user.uid;
+        const snapshot = await firebase.database().ref(`carOwners/${UID}`).once('value')
+        snap = await firebase.database().ref(`carOwners/${UID}/allCars`);
+        data = snapshot.val();
+        idNumber = Object.keys(data.allCars ? data.allCars : 0).length;
+        showStats(data);
     }
 })
 
-function errorMessage(msg = '') {
-    msg = (msg == '') ? 'Something went wrong, please try again' : msg
-    Swal.fire({
-            icon: 'error',
-            title: 'Opps...',
-            text: `${msg}`
-        })
-        .then(() => {
-            location.reload();
-        })
+var details=[];
+function showStats(data){
+    for(let i=0;i<idNumber;i++){
+        details.push(Object.values(data.allCars)[i])
+
+        var total = document.createElement('div');
+        total.classList.add("card-body");
+        total.setAttribute('id', `${details[i].carDetails.vehicleNumber}`);
+
+        var contentdiv = document.createElement('div');
+        contentdiv.innerHTML = `
+        <h3 class="text-primary"><u>${details[i].carDetails.brand} ${details[i].carDetails.model}</u></h3>
+        <div class="card card-body mb-3 mb-lg-5 border-secondary shadow-lg">
+            <div class="row gx-lg-4">
+            <div class="col-sm-6 col-lg-3">
+                <div class="media">
+                <div class="media-body">
+                    <h6 class="card-subtitle">Battery Voltage</h6>
+                    <span class="card-title h3" id="batteryVoltage">${details[i].allStatus.batteryVoltage}</span>
+                </div>
+                <span class="icon icon-sm icon-soft-secondary icon-circle ml-3">
+                    <i class="tio-book" aria-hidden="true"></i>
+                </span>
+                </div>
+                <div class="d-lg-none">
+                <hr>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 column-divider-sm">
+                <div class="media">
+                <div class="media-body">
+                    <h6 class="card-subtitle">Engine Temperature</h6>
+                    <span class="card-title h3" id="engineTemp">${details[i].allStatus.engineTemp}</span>
+                </div>
+                <span class="icon icon-sm icon-soft-secondary icon-circle ml-3">
+                    <i class="tio-website"></i>
+                </span>
+                </div>
+                <div class="d-lg-none">
+                <hr>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 column-divider-lg">
+                <div class="media">
+                <div class="media-body">
+                    <h6 class="card-subtitle">Suspension Level</h6>
+                    <span class="card-title h3" id="suspensionLevel">${details[i].allStatus.suspensionLevel}</span>
+                </div>
+                <span class="icon icon-sm icon-soft-secondary icon-circle ml-3">
+                    <i class="tio-trending-up"></i>
+                </span>
+                </div>
+                <div class="d-sm-none">
+                <hr>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-3 column-divider-sm">
+                <div class="media">
+                <div class="media-body">
+                    <h6 class="card-subtitle">Status</h6>
+                    <span class="card-title h3" id="status">Good</span>
+                </div>
+                <span class="icon icon-sm icon-soft-secondary icon-circle ml-3">
+                    <i class="tio-trending-up"></i>
+                </span>
+                </div>
+            </div>
+        </div>`;
+
+        total.appendChild(contentdiv);
+        document.getElementById("content").appendChild(total); 
+    }
 }
